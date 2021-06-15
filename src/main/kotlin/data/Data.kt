ef99a80a8534f7b043e95ecacafa8f7bd3d7254f
MaksimZotov <zotovmaksim1254@gmail.com>
2021-06-15 21:51:42 +0300

    data classes were updated

--- src/main/kotlin/data/Data.kt
+++ src/main/kotlin/data/Data.kt
@@ -3,14 +3,18 @@ package data
 interface Data
 
 class Name(val name: String): Data
+class RefusalTheName(val name: String): Data
+class AcceptingTheName(val name: String): Data
 class Invitation(val whoIsInvited: String): Data
 class AcceptingTheInvitation(val whoInvited: String): Data
+class PlayTheGame(): Data
 class RefusalTheInvitation(val whoInvited: String): Data
 class Question(val question: String, val answers: List<String>, val indexOfCorrectAnswer: Int): Data
 class RemainingTime(val time: Double): Data
-class Score(val player: Int, val anotherPlayer: Int)
+class Score(val player: Int, val anotherPlayer: Int): Data
 class Answer(val indexOfAnswer: Int): Data
 class LeavingTheGame(): Data
+class FinishTheGame(): Data
 class RequestToPlayAgain(): Data
 class RefusalToPlayAgain(): Data
 class Exit(): Data
